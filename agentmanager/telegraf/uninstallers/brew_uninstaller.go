63819762e8def023a7bc2903a5f37b1f297c2f72
Luis Rojas <luisrohu91@gmail.com>
2025-03-14 16:29:37 -0600

    stopping service before unistall to prevent zombie process.

--- agentmanager/telegraf/uninstallers/brew_uninstaller.go
+++ agentmanager/telegraf/uninstallers/brew_uninstaller.go
@@ -8,6 +8,11 @@ import (
 
 func BrewUninstall(updates chan<- string) error {
 	var err error
+
+	if err = utils.RunCommand("brew", []string{"services", "stop", "telegraf"}, updates); err != nil {
+		return fmt.Errorf("error stopping telegraf service: %v", err)
+	}
+
 	if err = utils.RunCommand("brew", []string{"uninstall", "telegraf"}, updates); err != nil {
 		return fmt.Errorf("error uninstalling telegraf service: %v", err)
 	}
